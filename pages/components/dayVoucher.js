b718ebfc02eae8b8ae7fe60dc94fb455c1a2b200
Elbert Bae <elbert.bae@gmail.com>
2019-06-10 21:31:18 -0700

    voucher component basically outlined, need to complete user auth and database work to proceed

--- pages/components/dayVoucher.js
+++ pages/components/dayVoucher.js
@@ -1,10 +1,31 @@
+import {useState, useEffect} from 'react';
 import '../../styles/components/dayVoucher.scss';
 
 export default function DayVoucher() {
+  const [voucher, setVoucher] = useState(null);
+  const createVoucher = () => {
+    // TODO CREATE DATABASE FOR VOUCHER, USER, AND EXPIRATION
+    console.log(new Date());
+  }
+  createVoucher();
   return (
     <div className="voucher-container">
-      <p className="subheader">Are you on the go and busy? Save a happy hour deal for later!</p>
-      <button type="button" className="get-voucher" onClick={() => { console.log('clicked') }}>Get Day Voucher</button>
+      { !voucher &&
+        <p className="subheader">Are you on the go and busy? Save a happy hour deal for later!</p>
+      }
+      { !voucher && 
+        <button type="button" className="get-voucher" onClick={() => { setVoucher(true) }}>Get Day Voucher</button>
+      }
+
+      { voucher && 
+        <p className="subheader">Redeem your voucher in person before it expires!</p>
+      }
+      { voucher && 
+        <button type="button" className="redeem-voucher" onClick={() => { console.log('clicked') }}>
+          <p className="subheader">Redeem Voucher</p>
+          <p className="voucher-timer">Expires at: 7:00pm</p>
+        </button>
+      }
     </div>
   )
 }
