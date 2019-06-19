d008eda91798a59e6a4251415a484385f01af6db
Elbert Bae <elbert.bae@gmail.com>
2019-06-18 17:26:23 -0700

    0auth initial test login page works

--- web/next.config.js
+++ web/next.config.js
@@ -5,5 +5,8 @@ const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   publicRuntimeConfig: {
     GOOGLE_API: process.env.GOOGLE_API,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_REDIRECT_URI: process.env.AUTH0_REDIRECT_URI
   }
 });
