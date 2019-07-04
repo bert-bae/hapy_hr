0c955b901f97e1eda576e232cd6767b1148f0f37
Adam Romeo <adam.r.romeo@gmail.com>
2019-07-04 13:13:58 -0700

    Setup getEstablishment stub in controller tests

--- server_api/test/establishment.controller.test.js
+++ server_api/test/establishment.controller.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const establishmentController = require('../controllers/EstablishmentController');
+const establishmentModel = require('../models/establishment');
 
 const res = {
     send: sinon.spy()
@@ -9,18 +10,23 @@ const res = {
 describe('EstablishmentController', () => {
     describe('getAllEstablishments', () => {
         afterEach(() => {
+            sinon.restore();
             res.send.resetHistory();
         });
 
-        it('should call res.send once', () => {
+        it('should call res.send with successful result', () => {
+            sinon.stub(establishmentModel, "getEstablishments").resolves([{id: 1}, {id: 2}])
             return establishmentController.getAllEstablishments({}, res, {}).then(data => {
                 expect(res.send.calledOnce).to.be.true;
+                expect(res.send.args[0][0].success).to.be.true;
             });
         });
 
-        it('should call res.send once again', () => {
+        it('should call res.send with unsuccessful result when getEstablishments throws an error', async () => {
+            sinon.stub(establishmentModel, "getEstablishments").throws();
             return establishmentController.getAllEstablishments({}, res, {}).then(data => {
                 expect(res.send.calledOnce).to.be.true;
+                expect(res.send.args[0][0].success).to.be.false;
             });
         });
     });
