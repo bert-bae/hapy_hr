1aa48894fa8f2a466b4f937784edb937b4ca53db
Adam Romeo <adam.r.romeo@gmail.com>
2019-07-04 23:54:54 -0700

    Add helper function to suppress console messages
    during tests and used this function inside all existing tests.

--- server_api/test/establishment.controller.test.js
+++ server_api/test/establishment.controller.test.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const suppressConsole = require('./utils/suppressConsole');
+
 const establishmentController = require('../controllers/EstablishmentController');
 const establishmentModel = require('../models/establishment');
 
@@ -22,8 +24,10 @@ describe('EstablishmentController', () => {
     describe('getAllEstablishments', () => {
 
         it('should call res.send with successful result', () => {
-            sinon.stub(establishmentModel, "getEstablishments").resolves([{id: 1}, {id: 2}]);
-            return establishmentController.getAllEstablishments({}, res, {}).then(data => {
+            sinon.stub(establishmentModel, 'getEstablishments').resolves([{id: 1}, {id: 2}]);
+
+            let result = suppressConsole(establishmentController.getAllEstablishments, [{}, res, {}]);
+            return result.then(() => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.true;
                 expect(res.send.args[0][0].error).to.be.null;
@@ -32,8 +36,10 @@ describe('EstablishmentController', () => {
         });
 
         it('should call res.send with unsuccessful result when getEstablishments throws an error', () => {
-            sinon.stub(establishmentModel, "getEstablishments").throws();
-            return establishmentController.getAllEstablishments({}, res, {}).then(data => {
+            sinon.stub(establishmentModel, 'getEstablishments').throws();
+            
+            let result = suppressConsole(establishmentController.getAllEstablishments, [{}, res, {}]);
+            return result.then(() => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.false;
                 expect(res.send.args[0][0].error).to.not.be.null;
@@ -44,8 +50,10 @@ describe('EstablishmentController', () => {
 
     describe('getSingleEstablishment', () => {
         it('should call res.send with successful result', () => {
-            sinon.stub(establishmentModel, "getSingleEstablishment").resolves({id: 1});
-            return establishmentController.getSingleEstablishment(req, res, {}).then(data => {
+            sinon.stub(establishmentModel, 'getSingleEstablishment').resolves({id: 1});
+
+            let result = suppressConsole(establishmentController.getSingleEstablishment, [req, res, {}]);
+            return result.then(() => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.true;
                 expect(res.send.args[0][0].error).to.be.null;
@@ -54,8 +62,10 @@ describe('EstablishmentController', () => {
         });
 
         it('should call res.send with unsuccessful result when getSingleEstablishment throws error', () => {
-            sinon.stub(establishmentModel, "getSingleEstablishment").throws();
-            return establishmentController.getSingleEstablishment(req, res, {}).then(data => {
+            sinon.stub(establishmentModel, 'getSingleEstablishment').throws();
+
+            let result = suppressConsole(establishmentController.getSingleEstablishment, [req, res, {}]);
+            return result.then(() => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.false;
                 expect(res.send.args[0][0].error).to.not.be.null;
