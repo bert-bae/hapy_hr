c4d657560958889867318bbe14b0e82c5b865744
Adam Romeo <adam.r.romeo@gmail.com>
2019-07-04 20:42:08 -0700

    Add expects to establishment controller tests

--- server_api/test/establishment.controller.test.js
+++ server_api/test/establishment.controller.test.js
@@ -15,10 +15,12 @@ describe('EstablishmentController', () => {
         });
 
         it('should call res.send with successful result', () => {
-            sinon.stub(establishmentModel, "getEstablishments").resolves([{id: 1}, {id: 2}])
+            sinon.stub(establishmentModel, "getEstablishments").resolves([{id: 1}, {id: 2}]);
             return establishmentController.getAllEstablishments({}, res, {}).then(data => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.true;
+                expect(res.send.args[0][0].error).to.be.null;
+                expect(res.send.args[0][0].establishment).to.deep.equal([{id: 1}, {id: 2}]);
             });
         });
 
@@ -27,6 +29,8 @@ describe('EstablishmentController', () => {
             return establishmentController.getAllEstablishments({}, res, {}).then(data => {
                 expect(res.send.calledOnce).to.be.true;
                 expect(res.send.args[0][0].success).to.be.false;
+                expect(res.send.args[0][0].error).to.not.be.null;
+                expect(res.send.args[0][0].establishment).to.be.null;
             });
         });
     });
