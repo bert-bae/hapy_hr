7f7751b91506d671570f54f489ffd430b9324afc
Elbert Bae <elbert.bae@gmail.com>
2019-07-14 08:48:30 -0700

    changed form components into controlled components

--- web/pages/establishment/list.js
+++ web/pages/establishment/list.js
@@ -1,7 +1,18 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import '../../styles/components/contactForm.scss'
 
 export default function List() {
+  const [applicantName, setApplicantName] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [province, setProvince] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [establishmentName, setEstablishmentName] = useState("");
+  const [establishmentDescription, setEstablishmentDescription] = useState("");
+
+
   return (
     <div className="page-container">
       <Head>
@@ -10,48 +21,99 @@ export default function List() {
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
       </Head>
       <div className="add-establishment-container">
-        <div className="form-group">
-          <div className="form-control restaurant-name">
-            <label for="location-name">Restaurant or Bar Name (required)</label>
-            <input class="form-input" type="text" maxLength="150" name="location-name" placeholder="Establishment Name"></input>
-          </div>
-          <div className="form-control">
-            <label for="location-name">Restaurant or Bar Description (required)</label>
-            <textarea class="form-input" type="text" maxLength="300" name="location-description" placeholder="Random Establishment specializes in providing a fun, casual, and lively environment with many cocktails, wide variety of craft beers, and games. Our specialty are our seafood dishes which go well with rum based cocktails any day of the week!"></textarea>
+        <div className="form-group about-you">
+          <div className="inner-container">
+            <div className="form-control">
+              <label for="location-name">Your Name (required)</label>
+              <input class="form-input" 
+                type="text" 
+                name="applicant-name" 
+                placeholder="E.g. Bert"
+                value={applicantName}
+                onChange={(e) => { setApplicantName(e.target.value); }}
+                ></input>
+            </div>
+            <div className="form-control">
+              <label for="location-name">Your Email (required)</label>
+              <input 
+                class="form-input" 
+                type="email" 
+                name="contact-email" 
+                placeholder="E.g. bert@happyr.ca"
+                value={contactEmail}
+                onChange={(e) => { setContactEmail(e.target.value); }}></input>
+            </div>
           </div>
         </div>
         <div className="form-group address-group">
           <div className="inner-container">
             <div className="form-control">
               <label for="address-line">Street Address (required)</label>
-              <input class="form-input" type="text" name="address-line" placeholder="E.g. 123 Granville Street"></input>
+              <input 
+                class="form-input" 
+                type="text" 
+                name="address-line" 
+                placeholder="E.g. 123 Granville Street"
+                value={streetAddress}
+                onChange={(e) => { setStreetAddress(e.target.value); }}></input>
             </div>
             <div className="form-control">
               <label for="location-name">City (required)</label>
-              <input class="form-input" type="text" name="city" placeholder="E.g. Vancouver"></input>
+              <input 
+                class="form-input" 
+                type="text" 
+                name="city" 
+                placeholder="E.g. Vancouver"
+                value={city}
+                onChange={(e) => { setCity(e.target.value); }}></input>
             </div>
           </div>
           <div className="inner-container">
             <div className="form-control">
               <label for="location-name">Province (required)</label>
-              <input class="form-input" type="text" name="province" placeholder="E.g. BC"></input>
+              <input 
+                class="form-input" 
+                type="text" 
+                name="province" 
+                placeholder="E.g. BC"
+                value={province}
+                onChange={(e) => { setProvince(e.target.value); }}></input>
             </div>
             <div className="form-control">
               <label for="location-name">Postal Code (required)</label>
-              <input class="form-input" type="text" maxLength="7" name="postal-code" placeholder="E.g. V9T 1V6"></input>
+              <input 
+                class="form-input" 
+                type="text" 
+                maxLength="7" 
+                name="postal-code" 
+                placeholder="E.g. V9T 1V6"
+                value={postalCode}
+                onChange={(e) => { setPostalCode(e.target.value); }}></input>
             </div>
           </div>
         </div>
-        <div className="form-group about-you">
-          <div className="inner-container">
-            <div className="form-control">
-              <label for="location-name">Your Name (required)</label>
-              <input class="form-input" type="text" name="applicant-name" placeholder="E.g. Bert"></input>
-            </div>
-            <div className="form-control">
-              <label for="location-name">Your Email (required)</label>
-              <input class="form-input" type="email" name="contact-email" placeholder="E.g. bert@happyr.ca"></input>
-            </div>
+        <div className="form-group">
+          <div className="form-control restaurant-name">
+            <label for="location-name">Restaurant or Bar Name (required)</label>
+            <input 
+              class="form-input" 
+              type="text" 
+              maxLength="150" 
+              name="location-name" 
+              placeholder="Establishment Name"
+              value={establishmentName}
+              onChange={(e) => { setEstablishmentName(e.target.value); }}></input>
+          </div>
+          <div className="form-control">
+            <label for="location-name">Restaurant or Bar Description (required)</label>
+            <textarea 
+              class="form-input" 
+              type="text" 
+              maxLength="300" 
+              name="location-description" 
+              placeholder="Random Establishment specializes in providing a fun, casual, and lively environment with many cocktails, wide variety of craft beers, and games. Our specialty are our seafood dishes which go well with rum based cocktails any day of the week!"
+              value={establishmentDescription}
+              onChange={(e) => { setEstablishmentDescription(e.target.value); }}></textarea>
           </div>
         </div>
       </div>
